0626a94b62d0fc27df67a8f619b32011f2db5ded
jbell1991 <joseph.louis.bell@gmail.com>
2020-03-03 12:57:08 -0500

    getting rid of large library pandas

--- app/model_db.py
+++ app/model_db.py
@@ -1,30 +1,30 @@
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 
-DB = SQLAlchemy()
+# DB = SQLAlchemy()
 
 
-class Songs(DB.Model):
-    #Tell SQLAlchemy what the table name is and if there's any table-specific arguments it should know about
-    id = DB.Column(DB.BigInteger, primary_key=True)
-    genre = DB.Column(DB.String(50))
-    artist_name = DB.Column(DB.String(50))
-    track_name = DB.Column(DB.String(100))
-    track_id = DB.Column(DB.String(50))
-    popularity = DB.Column(DB.Integer)
-    acousticness = DB.Column(DB.Float)
-    danceability = DB.Column(DB.Float)
-    duration_ms = DB.Column(DB.Integer)
-    energy = DB.Column(DB.Float)
-    instrumentalness = DB.Column(DB.Float)
-    key = DB.Column(DB.Integer)
-    liveness = DB.Column(DB.Float)
-    loudness = DB.Column(DB.Float)
-    mode = DB.Column(DB.Integer)
-    speechiness = DB.Column(DB.Float)
-    tempo = DB.Column(DB.Float)
-    time_signature = DB.Column(DB.Integer)
-    valence = DB.Column(DB.Float)
+# class Songs(DB.Model):
+#     #Tell SQLAlchemy what the table name is and if there's any table-specific arguments it should know about
+#     id = DB.Column(DB.BigInteger, primary_key=True)
+#     genre = DB.Column(DB.String(50))
+#     artist_name = DB.Column(DB.String(50))
+#     track_name = DB.Column(DB.String(100))
+#     track_id = DB.Column(DB.String(50))
+#     popularity = DB.Column(DB.Integer)
+#     acousticness = DB.Column(DB.Float)
+#     danceability = DB.Column(DB.Float)
+#     duration_ms = DB.Column(DB.Integer)
+#     energy = DB.Column(DB.Float)
+#     instrumentalness = DB.Column(DB.Float)
+#     key = DB.Column(DB.Integer)
+#     liveness = DB.Column(DB.Float)
+#     loudness = DB.Column(DB.Float)
+#     mode = DB.Column(DB.Integer)
+#     speechiness = DB.Column(DB.Float)
+#     tempo = DB.Column(DB.Float)
+#     time_signature = DB.Column(DB.Integer)
+#     valence = DB.Column(DB.Float)
 
-    def __repr__(self):
-        return '<Song {}>'.format(self.track_name)
+#     def __repr__(self):
+#         return '<Song {}>'.format(self.track_name)
