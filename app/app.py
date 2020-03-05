63b3568b937bc450fbfc1b5e458d88afb144c5b9
jbell1991 <joseph.louis.bell@gmail.com>
2020-03-04 20:24:31 -0500

    fixing everything

--- app/app.py
+++ app/app.py
@@ -4,8 +4,21 @@ import sqlite3
 import pandas as pd
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
+import numpy as np
+from sklearn import preprocessing  # for category encoder
+from sklearn.neighbors import NearestNeighbors
+from sklearn.model_selection import train_test_split
+from typing import List, Tuple
 
 DB = SQLAlchemy()
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/aguilargallardo/DS-Unit-2-Applied-Modeling/master/data/SpotifyFeatures.csv')
+
+
+
+
+
+
 
 
 class Songs(DB.Model):
@@ -35,15 +48,55 @@ class Songs(DB.Model):
         return '<Song {}>'.format(self.track_name)
 
 
-
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
         d[col[0]] = row[idx]
     return d
 
+
 def create_app():
     app = Flask(__name__)
+
+    df = pd.read_csv(
+        'https://raw.githubusercontent.com/aguilargallardo/DS-Unit-2-Applied-Modeling/master/data/SpotifyFeatures.csv')
+
+    def pre_process(df):
+        time_sig_encoding = {'0/4': 0, '1/4': 1,
+                             '3/4': 3, '4/4': 4,
+                             '5/4': 5}
+        key_encoding = {'A': 0, 'A#': 1, 'B': 2,
+                        'C': 3,  'C#': 4,  'D': 5,
+                        'D#': 6, 'E': 7, 'F': 8,
+                        'F#': 9, 'G': 10, 'G#': 11}
+        mode_encoding = {'Major': 0, 'Minor': 1}
+        df['key'] = df['key'].map(key_encoding)
+        df['time_signature'] = df['time_signature'].map(time_sig_encoding)
+        df['mode'] = df['mode'].map(mode_encoding)
+        # helper function to one hot encode genre
+
+        def encode_and_bind(original_dataframe, feature_to_encode):
+            dummies = pd.get_dummies(original_dataframe[[feature_to_encode]])
+            res = pd.concat([original_dataframe, dummies], axis=1)
+            return(res)
+        df = encode_and_bind(df, 'genre')
+        return df
+    
+
+    def closest_ten(df: pd.DataFrame, X_array: np.ndarray, song_id: int) -> List[Tuple]:
+        song = df.iloc[song_id]
+        X_song = X[song_id]
+        _, neighbors = neigh.kneighbors(np.array([X_song]))
+        return neighbors[0][1:]
+
+    processed_df = pre_process(df)
+
+    neigh = NearestNeighbors(n_neighbors=11)
+    features = list(processed_df.columns[4:])
+    X = processed_df[features].values
+
+    neigh.fit(X)
+
 
 
     @app.route('/populate')
@@ -53,12 +106,13 @@ def create_app():
         file_name = 'https://raw.githubusercontent.com/aguilargallardo/DS-Unit-2-Applied-Modeling/master/data/SpotifyFeatures.csv'
         df = pd.read_csv(file_name)
         DB = df.to_sql(con=engine, index_label='id',
-                name=Songs.__tablename__, if_exists='replace')
+                       name=Songs.__tablename__, if_exists='replace')
         return "Database has been made!"
 
     @app.route('/')
     def hello_world():
-        return "Hello World!"
+
+        return "Welcome to our Spotify API!  Route to /populate first to populate the database"
 
     #TODO make a route that takes in json data and converts it to match the database?
     @app.route('/user/data')
@@ -66,7 +120,7 @@ def create_app():
         pass
 
     #Model returns a list of songs and we return the top 10
-    @app.route('/songs', methods=['GET'])
+    @app.route('/songs', methods=['GET'])  # methods=['GET'])
     def get_songs():
         conn = sqlite3.connect('Spotify_Songs.db')
         conn.row_factory = dict_factory
@@ -75,5 +129,19 @@ def create_app():
             'SELECT track_name, artist_name, genre FROM songs LIMIT 10;').fetchall()
 
         return jsonify(all_songs)
+
+    @app.route('/track/<track_id>', methods=['GET'])  # /<track_id>
+    def track(track_id):
+        track_id = int(track_id)
+        conn = sqlite3.connect('Spotify_Songs.db')
+        conn.row_factory = dict_factory
+        curs = conn.cursor()
+        songlist = []
+        song_recs = closest_ten(df, X, track_id)
+        for idx in song_recs:
+            song = curs.execute(f'select * from Songs where id=={idx};').fetchall()
+            songlist.append(song)
+        
+        return jsonify(songlist)
 
     return app
